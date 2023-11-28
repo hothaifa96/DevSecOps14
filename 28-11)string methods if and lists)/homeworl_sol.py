42ddf3d41951c9e202c838fe02e4a09335ebd169
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-11-28 20:56:04 +0200

    if and conditions

---
+++ 28-11)string methods if and lists)/homeworl_sol.py
@@ -0,0 +1,33 @@
+# print('hello')
+
+# age = 25
+#
+# name = 'Barak obama'
+#
+# print('my name is :', name)
+
+# ============ str ============
+# 1
+# n = input('please enter a name: ')
+# print(type(n))
+# print('welcome ,', n)
+
+# 2
+# sen = input('please enter a sentence : ')
+#
+# print(sen.upper())
+# 3
+
+# 3
+# print(len(sen))  # count all chars  4 $ f
+
+# 4
+
+# ip = input('enter ip adress in this format xxx.xxx.xxx.xxx/xx')
+#
+# print(ip[:-3])  # all chars till the 4 th from the right
+# print(ip[-2:])  # last two chars
+
+# full_name = input('please enter your full name : ')
+# password = full_name[-3:] + full_name[:3]
+# print('your password is -> ', password)
