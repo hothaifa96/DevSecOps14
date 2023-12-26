b9b983c55e9b20026b307f8d1b63b74cad580b42
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-12-26 19:59:55 +0200

    args kwargs

--- 26-12(OOP-classes and objects)/main.py
+++ 26-12(OOP-classes and objects)/main.py
@@ -38,19 +38,67 @@
 # print(p1.name)
 
 
-class Person:
+class Person(object):
     # data
-    def __init__(self):
-        print(f'haha new object self = {self}')
-    # age = 20
-    # skin_color = 'navy'
-    # weight = 93.1
-    # name = 'koki'
-    # functionality
+
+    def __init__(self, name, gender, age, skin_color):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.skin_color = skin_color
+
+        # p4.name = name
+        # p4.gender = gender
+
+    def __len__(self):
+        return len(self.__dict__.keys())  # number of variables or attributes
+
+    def __add__(self, other):
+        return self.name + other.name
+
+    def __gt__(self, other):  # >
+        # gt ge le lt eq nq
+        return self.age > other.age
+
+    # def __le__(self, other):
+    #     return self.age <= other.age
+
+    # def __str__(self):
+    #     return f'the object name is {self.name}'
+
+    def __int__(self):
+        return self.age
+
+    def __float__(self):
+        return float(self.age)
+
+    def __bool__(self):
+        return True if len(self.name) > 4 else False
 
 
-p3 = Person()  # class name ()
-p4 = Person()
 
+p3 = Person('hodi', 'male', 11, 'white')  # class name ()
+p4 = Person('kokiiiiii', 'female', 100, 'black')
+p4.k = 11
+# p5 = Person('costa', 'male', 99, 'white')
+# p6 = Person('', '', 0, '')
+
+# p3.__dict__ = {'username': 'go'} # change the structure of the object
+# print(p3.__dict__)  # it's the way to extract a dict of attributes and there values
+#
+# print(p4.__dict__)
+# print(p5.__dict__)
+# print(p6.__dict__)
+
+# print(p3)
+# print(bool(p4))
+# print(len(p4))
+
+# s = str(p3) + '5'
+# print(s)
+
+print(p3 > p4)
+print(p4 < p3)
 print(p3)
-print(p4)
+
+# print(f'{p3.__class__} object at 0x{p3.__hash__()}')
