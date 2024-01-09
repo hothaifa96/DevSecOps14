91074d3b1c18606df28d841691e792e1c4f99c6e
hothaifa zoubi <hothaifazoubi@gmail.com>
2024-01-09 20:00:58 +0200

    flask

--- API's/api.py
+++ API's/api.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask
 
 # Flask is Class
@@ -9,5 +11,10 @@ app = Flask(__name__)  # building a flask app object using __init__()
 def gett_hello(): # the return of this function will be the response of the code
     return 'world !'
 
+@app.get('/math')
+def get_math():
+    x = random.randrange(1,100)
+    y = random.randrange(1,100)
+    return f'{x} + {y}  = ?'
 
 app.run(port=8881) # run the application
